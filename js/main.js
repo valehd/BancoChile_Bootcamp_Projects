d071c9794b8c9480b468dc8741919c0392c82592
valehd <valehd.92@gmail.com>
2026-05-23 00:50:42 -0400

    agrego div en contenedor de tareas, validador ok de html, ajusto estilos

--- js/main.js
+++ js/main.js
@@ -1,40 +1,34 @@
 //funcionalidad 1: alerta con input
 
 //obtener el texto del imput
-let texto= document.querySelector("#textoingresado")
+let texto = document.querySelector("#textoingresado");
 
 //obtener el boton
-let boton= document.querySelector("#crearTarea");
+let boton = document.querySelector("#crearTarea");
 
 //agregar el evento on click --> uso eventlistener
-boton.addEventListener("click", function(){
-    alert("agregando:"+ texto.value)
-})
-
+boton.addEventListener("click", function () {
+  alert("agregando:" + texto.value);
+});
 
 //funcionalidad 2: modificar p en tareas, dejarlo subrayado al hacer click
 
 //capturo el primer p de la clase tarea
-let tareaClick= document.querySelectorAll(".tarea p")
+let tareaClick = document.querySelectorAll(".tarea p");
 
-tareaClick.forEach(function(tarea){
-    tarea.addEventListener("click",function(){
-         tarea.style.textDecoration = "line-through";
-    })
-})
-
+tareaClick.forEach(function (tarea) {
+  tarea.addEventListener("click", function () {
+    tarea.style.textDecoration = "line-through";
+  });
+});
 
 //funcionalidad 3: cursor sobre icono, cambia a otro icono
 
-function  cambiarImagen(){
-
-
-let imgNotificacion= document.getElementById("notificacion")
-if(imgNotificacion.src.includes("notificaciones-1.png")) {
-    imgNotificacion.src="imgs/notificaciones-2.png";
-}else { imgNotificacion.src="imgs/notificaciones-1.png";
-
-}}
-
-
-
+function cambiarImagen() {
+  let imgNotificacion = document.getElementById("notificacion");
+  if (imgNotificacion.src.includes("notificaciones-1.png")) {
+    imgNotificacion.src = "imgs/notificaciones-2.png";
+  } else {
+    imgNotificacion.src = "imgs/notificaciones-1.png";
+  }
+}
